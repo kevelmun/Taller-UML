89aa977a007baa933c99372bf4ef39778733498f
lnsalaza <lnsalaza@espol.edu.ec>
2021-06-24 16:00:51 -0500

    V1.9

--- TallerUML/src/main/java/com/mycompany/talleruml/blueFolder/HistoriaClinica.java
+++ TallerUML/src/main/java/com/mycompany/talleruml/blueFolder/HistoriaClinica.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package blueFolder;
+package com.mycompany.talleruml.blueFolder;
 
 import java.util.ArrayList;
 
