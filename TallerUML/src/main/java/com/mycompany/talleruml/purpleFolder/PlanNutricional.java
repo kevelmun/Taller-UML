c720aaa02e2eeebcf98d1efd7921fa5f6ab663fe
Juan Quimí <juafrqui@espol.edu.ec>
2021-06-24 12:41:48 -0500

    V1.5

---
+++ TallerUML/src/main/java/com/mycompany/talleruml/purpleFolder/PlanNutricional.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.talleruml.purpleFolder;
+
+import com.mycompany.talleruml.purpleFolder.Receta;
+/**
+ *
+ * @author NewPc
+ */
+public class PlanNutricional {
+    protected String descripcion;
+    
+    public PlanNutricional(String descripcion){
+        this.descripcion = descripcion;
+    }
+}
---
+++ TallerUML/src/main/java/com/mycompany/talleruml/purpleFolder/Receta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.talleruml.purpleFolder;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import com.mycompany.talleruml.pinkFolder.Paciente;
+import com.mycompany.talleruml.pinkFolder.Doctor;
+/**
+ *
+ * @author NewPc
+ */
+public class Receta {
+    protected ArrayList<Medicamento> litaMedicamentos;
+    protected LocalDateTime fecha;
+    
+}
